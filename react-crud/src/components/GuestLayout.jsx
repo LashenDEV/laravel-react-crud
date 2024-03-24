a67ec5eecd9a6997288f98b9355acdf3c7f6e716
LashenDEV <kariyawasamlashen@gmail.com>
2024-03-24 14:34:13 +0530

    Dashboard Layouts, Login and Singup Frontend Done

---
+++ react-crud/src/components/GuestLayout.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {Navigate, Outlet} from "react-router-dom";
+import {useStateContext} from "../contexts/ContextProvider.jsx";
+
+function GuestLayout() {
+    const  {token} = useStateContext()
+
+    if(token) {
+        return <Navigate to="/" />
+    }
+
+    return (
+        <div>
+            <Outlet/>
+        </div>
+    );
+}
+
+export default GuestLayout;
